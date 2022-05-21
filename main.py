07055d9f0feef358d80bc5e3bf863373689b1a75
Varunmnx <varun.cheriyath@gmail.com>
2022-05-22 00:08:59 +0530

    tested pydantic library

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from random import sample
+# from random import sample
+# from telnetlib import STATUS
+from typing import Optional
 from fastapi import Body, FastAPI
-
+from fastapi.params import Body
+from pydantic import BaseModel
 app = FastAPI()
+class postparam(BaseModel):
+    item: str
+    content:str
+    status: True
+    rating: Optional[int] =None
+
 
 @app.get("/")
 async def root():
@@ -15,3 +24,7 @@ async def profile():
 @app.post("/createposts")
 def post(sample:dict=Body):
     return {"data":f"{sample['item']}and {sample['content']}"}
+@app.post("/createpost")
+def post(postings:postparam):
+    return {"data":f"{postings.item} and content of that is {postings.content}"}
+        
