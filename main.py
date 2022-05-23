22000e2d7df917ce1625df1ddd3c5f3ece0f4ccf
Varunmnx <varun.cheriyath@gmail.com>
2022-05-23 23:04:37 +0530

    how to query and how to throw statuscode if the query is unavailable problem of importing from different file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# from random import sample
-# from telnetlib import STATUS
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI,Response,status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+
+# from requests import Response
 app = FastAPI()
 class postparam(BaseModel):
     item: str
@@ -13,7 +13,15 @@ class postparam(BaseModel):
     rating: Optional[int] =None
     
 
-temparraydatabase =[{" item ":" Book "," content ":" unruled Books "},{" item ":" Pen "," content ":" Ball pen "}]
+temparraydatabase =[{" item ":" Book "," content ":" unruled Books ","id":1},{" item ":" Pen "," content ":" Ball pen "}]
+def get_post(id):
+    for element in temparraydatabase:
+        if element['id'] == id:
+             return element
+        else:
+            return None     
+
+
 
 @app.get("/")
 async def root():
@@ -27,10 +35,18 @@ async def profile():
 # @app.post("/createposts")
 # def post(sample:dict=Body):
 #     return {"data":f"{sample['item']}and {sample['content']}"}
+
+#post a data and giving unique id to each of the element saved
 @app.post("/createposts")
 def post(postings:postparam):
     asdic = postings.dict()
     asdic['id'] = randrange(0,100000)
     temparraydatabase.append(asdic)
     return {"data": temparraydatabase}
-        
+
+@app.get("/profile/{id}")
+def get_posts(id:int):
+    post = get_post(id)
+    if not post:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"id:{id} was not found.Thankyou for searching" )
+    return {"posts detail": post}
